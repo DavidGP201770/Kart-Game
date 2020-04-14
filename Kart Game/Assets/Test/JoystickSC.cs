b9e063ba49a16bb0f379cd3a352864bed61317ed
DavidGP201770 <davicho201170@gmail.com>
2020-04-14 13:58:59 -0500

    Test scene update
    Test scene update

---
+++ Kart Game/Assets/Test/JoystickSC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickSC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected Joystick joystick;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        var rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rbody = GetComponent<Rigidbody>();
+        rbody.velocity = new Vector3(joystick.Horizontal * 7f, rbody.velocity.y, joystick.Vertical * 7f);
+    }
+}
