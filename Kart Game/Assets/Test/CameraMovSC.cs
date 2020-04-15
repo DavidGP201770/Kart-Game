cedef6868297ade5be92fe0a740e838448956327
DavidGP201770 <davicho201170@gmail.com>
2020-04-15 07:43:53 -0500

    Update Test Scene
    Update Test Scene

---
+++ Kart Game/Assets/Test/CameraMovSC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovSC : MonoBehaviour
+{
+    public float RotationSpeed = 10.0f;
+    public Transform Target, Player;
+    float MouseX, MouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraControl();
+    }
+
+    void CameraControl()
+    {
+        MouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        //MouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        //MouseY = Mathf.Clamp(MouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(0, MouseX,0);
+        Player.rotation = Quaternion.Euler(0, MouseX, 0);
+    }
+}
